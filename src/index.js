25a6b07b21cac058970c9ae506dc9b11973b8089
Jose Masri <josemasri222@gmail.com>
2020-08-12 23:25:08 -0500

    Changed setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-import './main.css';
-import styles from './index.module.css';
-
-const h1 = document.querySelector('h1');
-h1.classList.add(styles.h1);
+import './styles/style.scss';
+import styles from './styles/index.module.css';
 
 async function print() {
   // Here we are using dynamic import
