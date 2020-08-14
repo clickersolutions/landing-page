ffd7ffcc8889c41d5b9f5838aa5e5714fa947621
Gerardo Torres <gerardotorres@MacBook-Air.local>
2020-08-13 23:41:07 -0500

    mejoras

--- src/animations.js
+++ src/animations.js
@@ -3,6 +3,9 @@ import anime from 'animejs/lib/anime.es.js';
 
 export const startAnimations = () => {
     animateForm();
+    animateNavbar();
+    animateTitle();
+    blur();
 };
 
 const animateForm = () => {
@@ -12,3 +15,27 @@ const animateForm = () => {
     });
 };
 
+const animateNavbar = () => {
+    anime({
+        targets: 'nav',
+        translateX: [400, 0]
+    });
+};
+
+const animateTitle = () => {
+    anime({
+        targets: '#title',
+        translateX: [-200, 0]
+    });
+};
+
+const blur = () => {
+    var targetElm = document.querySelector('#contactForm');
+    anime({
+        duration: 1000,
+        direction: 'alternate',
+        update: function(anim){
+          targetElm.style.filter = 'blur(' + 20 * anim.progress / 100 + 'px)'
+        }
+      });
+}
