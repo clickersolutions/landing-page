be02d8dccb2018f13f42f51a87221685d950a061
Gerardo Torres <gerardotorres@MacBook-Air.local>
2020-08-17 21:29:31 -0500

    cambios

--- src/index.js
+++ src/index.js
@@ -10,3 +10,17 @@ form.addEventListener("submit", (e) => {
   const message = document.querySelector("#message");
 });
 
+
+const body = document.querySelector('body');
+
+body.addEventListener("scroll", (e) => {
+  e.preventDefault();
+  const stop = body.scrollTop;
+  const div1 = document.querySelector('#seccionAliados');
+  const navElements = document.querySelectorAll('nav li');
+  if(stop > div1.offsetTop -  50){
+    navElements.forEach(elem => elem.classList.add('text-black'));
+  } else {
+    navElements.forEach(elem => elem.classList.remove('text-black'));
+  }
+});
